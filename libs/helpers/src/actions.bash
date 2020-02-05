8538856d80102da3d7da750d509ea79c10425c16
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-05 08:29:52 +0530

    Code Refractoring

--- libs/helpers/src/actions.bash
+++ libs/helpers/src/actions.bash
@@ -18,12 +18,16 @@ function update_cloud_init_template() {
 function ssh_config_agent_on_host(){
     # Ensure SSH Agent Is Running in Background
     eval "$(ssh-agent -s)"
+
+    #              Changes Host Settings               #
+    #+++++++++++++++++++++++++++++++++++++++++++++++++ #
     # Automatically load keys into the ssh-agent
     yes | cp -rf ~/.ssh/config ~/.ssh/config.bak
     cat $SSH_CONFIG > ~/.ssh/config
     # Add your SSH private key to the ssh-agent and store your passphrase in the keychain
-    #ssh-add -k ~/.ssh/id_rsa
+    ssh-add -k ~/.ssh/id_rsa
     ssh-add -k ~/.ssh/multipass/id_rsa_$VM_NAME
+    #+++++++++++++++++++++++++++++++++++++++++++++++++ #
 
     IP=$(multipass info $VM_NAME | grep IPv4 | awk '{print $2}')
     # delete old key from known_hosts
@@ -31,9 +35,10 @@ function ssh_config_agent_on_host(){
 
     # rescan the Host and add it to the known_hosts
     ssh-keyscan -t rsa $IP >> ~/.ssh/known_hosts
-    echo "\nHost $VM_NAME\n\tHostname ${IP}\n\tUser ubuntu\n\tIdentityFile ~/.ssh/multipass/id_rsa_$VM_NAME" >> ~/.ssh/config
-    echo "SSH Agemnt Configured Successfully"
-    echo "Next: ssh $VM_NAME or ssh $VM_NAME@$IP"
+    # updating local ssh configuration.
+    echo -e "Host $VM_NAME\n\tHostname ${IP}\n\tUser ubuntu\n\tIdentityFile ~/.ssh/multipass/id_rsa_$VM_NAME\n" >> ~/.ssh/config 
+    echo "SSH Agent Configured Successfully"
+    echo "Next: ssh $VM_NAME or ssh ubuntu@$IP"
 }
 
 
@@ -53,35 +58,8 @@ function destroy(){
 
     # Reset with Defaults
     cat $SSH_CONFIG > ~/.ssh/config
-    is_vm_running=$(multipass ls | grep $VM_NAME |awk '{ print $2}' | wc -l )
+    multipass delete $VM_NAME && multipass purge
 
-    if [ $is_vm_running -eq 1 ];then
-        multipass delete $VM_NAME && multipass purge
-    fi 
-}
-
-function menu() {
-    tput clear
-    title=$1
-    in=$2
-    arr=$(echo $in | tr "," "\n")
-    x=1
-    y=1
-    
-    tput cup $y $x
-    tput rev;echo " $title "; tput sgr0
-    x=$((x+${#title}+4))
-    i=0
-    for n in $arr
-    do
-        tput cup $(( y+$i )) $x
-            str="$n" 
-            echo "$((i+1)). $str"
-        i=$((i+1))
-    done
-    tput cup $(( y+$i+1 )) $((x-$((${#title}+4))))
-    read -p "Enter your choice [1-$i] " choice
-    return $choice
 }
 
 function comment_line(){
@@ -95,5 +73,3 @@ function uncomment_line(){
     file_to_edit=$2
     sed -i '' -e "/$pattern/s/^#//g"  file_to_edit
 }
-
-sed -i '/<pattern>/s/^#//g' file #uncomment
--- multipass.bash
+++ multipass.bash
@@ -5,7 +5,9 @@
 # https://discourse.ubuntu.com/t/troubleshooting-networking-on-macos/12901
 # https://github.com/lucaswhitaker22/bash_menus/blob/master/bash_menus/demo.sh
 
+source "workspace.env"
 source "$WORKSPACE/libs/helpers/load"
+
 
 ### Arguments with Default Values
 VM_NAME=${1:-"bizapps"}
@@ -19,19 +21,48 @@ SSH_KEY="id_rsa_${VM_NAME}"
 CLOUD_INIT_TEMPLATE="config/cloud-init-template.yaml"
 CLOUD_INIT_FILE="config/${VM_NAME}-cloud-init.yaml"
 
+function menu() {
+    tput clear
+    title=$1
+    in=$2
+    arr=$(echo $in | tr "," "\n")
+    x=1
+    y=1
+    
+    tput cup $y $x
+    tput rev;echo " $title "; tput sgr0
+    x=$((x+${#title}+4))
+    i=0
+    for n in $arr
+    do
+        tput cup $(( y+$i )) $x
+            str="$n" 
+            echo "$((i+1)). $str"
+        i=$((i+1))
+    done
+    tput cup $(( y+$i+1 )) $((x-$((${#title}+4))))
+    read -p "Enter your choice [1-$i] " choice
+    return $choice
+}
+
 help(){
-    menu "Multipass Manager" "Provision,SSH,Destroy"
+    menu "Multipass Manager" "Provision,SSH-Multipass,SSH-Host, Destroy"
     choice=$?
     case $choice in 
         1) 
             provision 
             ;;
         2) 
-            # Commented out Line is for self learning in configuring ssh
-            # ssh_config_agent_on_host && ssh bizapps # For Understanding SSH Configuration
             multipass shell $VM_NAME
             ;;
-        3) destroy
+        3) 
+            multipass info $VM_NAME || exit 1 #Exit if VM Not Running
+            ssh_config_agent_on_host
+            ;;
+        4) 
+            multipass info $VM_NAME || exit 1 #Exit if VM Not Running
+            destroy
+            echo "$VM_NAME Destroyed"
             ;;
         *) 
             echo "Invalid Input"
