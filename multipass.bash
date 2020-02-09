6d710b62df63c9f4092cdc61a67462d091ec2d9a
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-09 15:25:09 +0530

    TDD

--- multipass.bash
+++ multipass.bash
@@ -7,13 +7,15 @@
 
 source "instance.env"
 source "workspace.env"
-source "$WORKSPACE/src/multipass/load"
+
+# shellcheck disable=SC1090
+source "$WORKSPACE/src/load"
 
 function menu() {
     tput clear
     title=$1
     in=$2
-    arr=$(echo $in | tr "," "\n")
+    arr=$(echo "$in" | tr "," "\n")
     x=1
     y=1
     
@@ -23,14 +25,17 @@ function menu() {
     i=0
     for n in $arr
     do
+        # shellcheck disable=SC2004
         tput cup $(( y+$i )) $x
             str="$n" 
             echo "$((i+1)). $str"
         i=$((i+1))
     done
+    # shellcheck disable=SC2004
     tput cup $(( y+$i+1 )) $((x-$((${#title}+4))))
+    # shellcheck disable=SC2162
     read -p "Enter your choice [1-$i] " choice
-    return $choice
+    return "$choice"
 }
 
 choose_action_from_menu(){
@@ -38,22 +43,22 @@ choose_action_from_menu(){
     choice=$?
     case $choice in 
         1) .
-            start=`date +%s`
+            start=$(date +%s)
             provision 
-            end=`date +%s`
+            end=$(date +%s)
             runtime=$((end-start))
             display_time $runtime
             ;;
         2) 
-            multipass shell $VM_NAME
+            multipass shell "$VM_NAME"
             ;;
         3) 
-            multipass info $VM_NAME || exit 1 #Exit if VM Not Running
+            multipass info "$VM_NAME" || exit 1 #Exit if VM Not Running
             ssh_config_agent_on_host
             eval "$(ssh-agent -s)"
             ;;
         4) 
-            multipass info $VM_NAME || exit 1 #Exit if VM Not Running
+            multipass info "$VM_NAME" || exit 1 #Exit if VM Not Running
             destroy
             echo "$VM_NAME Destroyed"
             ;;
