61ccbcaf413c5716b8c523e887c700a446e7496b
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-10 07:18:47 +0530

    Fix ShellChecks

--- src/multipass/checks.bash
+++ src/multipass/checks.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# Checks if the VM_NAME environment variable is not Empty or Null
 function check_vm_name_required() {
-    # Check if vm name is passed as a parameter to script or provided in workspace.env - if not exit
-    [ -z "$VM_NAME" ] && echo -e "$VM_NAME_NOT_SET_ERR_MSG" && exit 1 || echo -e "VM Name :: $VM_NAME"
+    # Check if vm name is passed as a parameter to script or provided in workspace.env - if not raise error
+    [ -z "$VM_NAME" ] && raise "$VM_NAME_NOT_SET_ERR_MSG" && return 1 || echo -e "VM Name :: $VM_NAME"
 }
 
+# Checks if required env variables for workspace is all set
 function check_required_workspace_env_vars() {
   local required_env="WORKSPACE SSH_CONFIG SSH_KEY_PATH CLOUD_INIT_TEMPLATE "
   local missing_env_vars=""
@@ -19,6 +21,7 @@ function check_required_workspace_env_vars() {
   done
 }
 
+# Checks if required env variables for instance is all set
 function check_required_instance_env_vars() {
   local required_env="VM_NAME DOMAIN CPU MEMORY DISK"
   local missing_env_vars=""
@@ -33,17 +36,19 @@ function check_required_instance_env_vars() {
   done
 }
 
-
+# Checks if required env variables for instance is all set
 function raise(){
   echo "${1}" >&2
 }
 
+# Wrapper To Aid TDD
 function run_main(){
     check_vm_name_required
     check_required_workspace_env_vars 
     check_required_instance_env_vars
 }
 
+# Wrapper To Aid TDD
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
   run_main
