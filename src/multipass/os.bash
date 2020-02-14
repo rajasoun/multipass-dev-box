8c003b7b5f0361d0db4ec6e492420a10c45e897e
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-14 07:46:31 +0530

    Add Tests

--- src/multipass/os.bash
+++ src/multipass/os.bash
@@ -44,29 +44,6 @@ function file_contains_text {
   grep -q "$text" "$file"
 }
 
-# Returns os name windows, mac, linux or unknown
-function os(){
-  UNAME=$( command -v uname)
-  case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
-    linux*)
-      printf 'linux\n'
-      ;;
-    darwin*)
-      printf 'mac\n'
-      ;;
-    msys*|cygwin*|mingw*)
-      # or possible 'bash on windows'
-      printf 'windows\n'
-      ;;
-    nt|win*)
-      printf 'windows\n'
-      ;;
-    *)
-      printf 'unknown\n'
-      ;;
-  esac
-}
-
 function run_main() {
     lls "$@"
     os_command_is_installed "$@"
