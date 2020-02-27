b64ea7f24484025aac7a5863ebb601a7e2f2c4a0
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-27 12:02:59 +0530

    Log Support

---
+++ dev-tools/ReadMe.md
@@ -0,0 +1,14 @@
+# Dev Tools Sandbox
+
+Dev tools sandbox mimics tools in production as close as possible enabling developers to experiment and 
+learn the same toolset for logging, container management and server monitoring 
+
+The choice of the tools are aimed towards simplicity and of apps of small to medium size.
+
+|      Purpose        |      Tool      |
+|:--------------------|:---------------|
+|Container  Management|[portainer]     |
+|Centralized Logging  |[rsyslog]       |
+
+[rsyslog]: http://manpages.ubuntu.com/manpages/bionic/man8/rsyslogd.8.html
+[portainer]: https://portainer.readthedocs.io/en/stable/deployment.html
--- dev-tools/sandbox.bash
+++ dev-tools/sandbox.bash
@@ -4,72 +4,90 @@ set -eo pipefail
 IFS=$'\n\t'
 TOOLS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if ! [ -x "$(command -v docker)" ]; then
-  echo 'Error: docker is not installed.' >&2
-  exit 1
-fi
+source "$(dirname "${BASH_SOURCE[0]}")/../src/multipass/os.bash"
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
+function check_preconditions() {
+  if ! [ -x "$(command -v docker)" ]; then
+    echo 'Error: docker is not installed.' >&2
+    exit 1
+  fi
 
-function enter(){
-      case $container_name in
-          rsyslog)
-              echo "Entering /bin/bash session in the rsyslog container..."
-              docker-compose -f "$TOOLS_DIR/rsyslog.yml" exec rsyslog bash
-              ;;
-          *)
-              echo "sandbox enter (ryslog)"
-              ;;
-      esac
+  if ! [ -x "$(command -v docker-compose)" ]; then
+    echo 'Error: docker-compose is not installed.' >&2
+    exit 1
+  fi
+}
+
+function enter() {
+  case $container_name in
+  rsyslog)
+    echo "Entering /bin/bash session in the rsyslog container..."
+    docker-compose -f "$TOOLS_DIR/rsyslog.yml" exec rsyslog bash
+    ;;
+  *)
+    echo "sandbox enter (ryslog)"
+    ;;
+  esac
 }
 
 function logs() {
-        case $container_name in
-            rsyslog)
-                echo "rsyslog Logs ..."
-                docker-compose -f "$TOOLS_DIR/rsyslog.yml"  logs -f
-                ;;
-            *)
-                 echo "sandbox logs (rsyslog)"
-                ;;
-        esac
+  case $container_name in
+  rsyslog)
+    echo "rsyslog Logs ..."
+    docker-compose -f "$TOOLS_DIR/rsyslog.yml" logs -f
+    ;;
+  portainer)
+    echo "portainer Logs ..."
+    docker-compose -f "$TOOLS_DIR/portainer.yml" logs -f
+    ;;
+  *)
+    echo "sandbox logs (rsyslog | portainer)"
+    ;;
+  esac
+}
+
+function send_msg_to_syslog(){
+  docker run --rm --log-driver syslog --log-opt syslog-address=udp://$(get_local_ip):5514 alpine echo "$@"
 }
 
 function sandbox() {
-    case $action in
-        up)
-            echo "Spinning up Docker Images..."
-            echo "If this is your first time starting sandbox this might take a minute..."
-            docker-compose -f "$TOOLS_DIR/rsyslog.yml" up -d --build
-            ;;
-        down)
-            echo "Stopping sandbox containers..."
-            docker-compose -f "$TOOLS_DIR/rsyslog.yml" down -v
-            rm -fr "$TOOLS_DIR/logs/syslog"
-            ;;
-        enter)
-            enter "$@"
-            ;;
-        logs)
-            logs "$@"
-            ;;
-        *)
-            cat <<-EOF
+  case $action in
+  up)
+    echo "Spinning up Docker Images..."
+    echo "If this is your first time starting sandbox this might take a minute..."
+    docker-compose -f "$TOOLS_DIR/rsyslog.yml" -f "$TOOLS_DIR/portainer.yml" up -d --build
+    ;;
+  send_msg)
+    send_msg_to_syslog "$@"
+    ;;
+  down)
+    echo "Stopping sandbox containers..."
+    docker-compose -f "$TOOLS_DIR/rsyslog.yml" -f "$TOOLS_DIR/portainer.yml"  down -v  --remove-orphans
+    echo "Removing file $TOOLS_DIR/logs/syslog ..."
+    rm -fr "$TOOLS_DIR/logs/syslog"
+    ;;
+  enter)
+    enter "$@"
+    ;;
+  logs)
+    logs "$@"
+    ;;
+  *)
+    cat <<-EOF
 sandbox commands:
 ----------------
   up                 -> spin up the dev-tools sandbox environment
   down               -> tear down the dev-tools sandbox environment
-  enter (rsyslog)    -> enter the specified container
-  logs  (rsyslog)    -> stream logs for the specified container
+  send_msg           -> sends message to syslog
+  enter (rsyslog | portainer)    -> enter the specified container
+  logs  (rsyslog | portainer)    -> stream logs for the specified container
 EOF
-            ;;
-    esac
+    ;;
+  esac
 }
 
 action=$1
 container_name=$2
 
+check_preconditions
 sandbox "$@"
