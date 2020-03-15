d70de250a1ffb491be21d28f06b9ea7fab337933
Raja. Soundaramourty <rajasoun@cisco.com>
2020-03-16 02:06:18 +0530

    Traefiek support

--- dev-tools/assist.bash
+++ dev-tools/assist.bash
@@ -22,8 +22,8 @@ function help(){
 IP="$(get_local_ip)"
 TOOLS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #COMPOSE_FILES=" -f portainer.yml -f rsyslog.yml -f loki.yml -f grafana.yml -f webtail.yml"
-COMPOSE_FILES="  -f $TOOLS_DIR/portainer.yml -f $TOOLS_DIR/rsyslog.yml  -f $TOOLS_DIR/loki.yml -f $TOOLS_DIR/grafana.yml -f $TOOLS_DIR/webtail.yml -f $TOOLS_DIR/traefik.yml"
-SERVICES=(traefik portainer grafana webtail)
+COMPOSE_FILES="  -f $TOOLS_DIR/portainer.yml -f $TOOLS_DIR/rsyslog.yml  -f $TOOLS_DIR/loki.yml -f $TOOLS_DIR/grafana.yml -f $TOOLS_DIR/webtail.yml -f $TOOLS_DIR/traefik.yml -f $TOOLS_DIR/traefik-fa.yml -f $TOOLS_DIR/whoami.yml"
+SERVICES=(traefik portainer grafana webtail whoami)
 
 export IP
 export TOOLS_DIR
